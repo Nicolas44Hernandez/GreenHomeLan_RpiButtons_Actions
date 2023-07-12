8d44dcb85dcc68c11c1b46f676072a67cb7c196d
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-07-12 10:30:48 +0200

    check buttons status

--- server_buttons/server/interfaces/gpio_interface/service.py
+++ server_buttons/server/interfaces/gpio_interface/service.py
@@ -2,10 +2,15 @@
 GPIO interface service
 """
 import logging
+from timeloop import Timeloop
+from datetime import timedelta
 from typing import Iterable
 import RPi.GPIO as GPIO
 import adafruit_matrixkeypad as Keypad
+
 logger = logging.getLogger(__name__)
+
+buttons_status_timeloop = Timeloop()
 
 ROWS=2
 COLS=2
@@ -33,14 +38,25 @@ class GpioButtonMatrixInterface:
         self.keypad = Keypad.Matrix_Keypad(rowsPins, colsPins, keys)
 
 
-    def check_button_pressed(self):
-        """Return the key of the button pressed, if not button"""
-        logger.info("Checking if button is pressed")
-        keys = self.keypad.pressed_keys
-        if keys:
-            logger.info(f"Buttons pressed: {keys}")
-            return keys
-        return None
+    def schedule_buttons_status_polling(self):
+        """Schedule the buttons status polling"""
+
+        # Buttons status polling job
+        @buttons_status_timeloop.job(interval=timedelta(milliseconds=50))
+        def check_buttons_pressed():
+            logger.info(f"Launch buttons status polling")
+            keys = self.keypad.pressed_keys
+            if keys:
+                logger.info(f"Buttons pressed: {keys}")
+
+    # def check_button_pressed(self):
+    #     """Return the key of the button pressed, if not button"""
+    #     logger.info("Checking if button is pressed")
+    #     keys = self.keypad.pressed_keys
+    #     if keys:
+    #         logger.info(f"Buttons pressed: {keys}")
+    #         return keys
+    #     return None
 
 
 
